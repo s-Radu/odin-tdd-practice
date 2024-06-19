b4ae53c8d12e5b2b6351c8d154532797761f12ed
lil.void <tsraduu@gmail.com>
2024-06-19 19:19:51 +0100

    tets added

---
+++ tests/calculator.test.js
@@ -0,0 +1,22 @@
+const {add, substract, multiply, divide} = require('../src/calculator');
+// Addition
+
+test('Sum two numbers', () => {
+	expect(add(2, 2)).toBe(4);
+});
+
+test('Sum negative numbers', () => {
+	expect(add(-7, -5)).toBe(-12);
+});
+
+test('Refuse anything that is not a number', () => {
+	expect(add('a' + 2)).toBe('Only numbers accepted!');
+});
+
+test('Refuse undefined', () => {
+	expect(add(undefined + 4)).toBe('Only numbers accepted!');
+});
+
+test('Refuse null', () => {
+	expect(add(null + 4)).toBe('Only numbers accepted!');
+});
