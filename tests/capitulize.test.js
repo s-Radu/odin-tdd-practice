be52c21518c55f5e570bd8c3a035d05513a405ea
lil.void <tsraduu@gmail.com>
2024-06-24 17:25:57 +0100

    Refractored capitulize tests to look a bit better

--- tests/capitulize.test.js
+++ tests/capitulize.test.js
@@ -1,29 +1,30 @@
 const capitulize = require('../src/capitulize');
+describe('Capitulaze tests', () => {
+	test('Check for number', () => {
+		expect(capitulize(12345)).toBe('Only strings accepted!');
+	});
 
-test('Check for number', () => {
-	expect(capitulize(12345)).toBe('Only strings accepted!');
-});
+	test('Capitulize first letter', () => {
+		expect(capitulize('sarmale')).toBe('Sarmale');
+	});
 
-test('Capitulize first letter', () => {
-	expect(capitulize('sarmale')).toBe('Sarmale');
-});
+	test('Capitulize empty string', () => {
+		expect(capitulize('')).toBe('');
+	});
 
-test('Capitulize empty string', () => {
-	expect(capitulize('')).toBe('');
-});
+	test('Capitulize string starting with a number', () => {
+		expect(capitulize('1da?')).toBe('1da?');
+	});
 
-test('Capitulize string starting with a number', () => {
-	expect(capitulize('1da?')).toBe('1da?');
-});
+	test('Test for null ', () => {
+		expect(capitulize(null)).toBe('Only strings accepted!');
+	});
 
-test('Test for null ', () => {
-	expect(capitulize(null)).toBe('Only strings accepted!');
-});
+	test('Test for undefined ', () => {
+		expect(capitulize(undefined)).toBe('Only strings accepted!');
+	});
 
-test('Test for undefined ', () => {
-	expect(capitulize(undefined)).toBe('Only strings accepted!');
-});
-
-test('Test for capitulized input ', () => {
-	expect(capitulize('Sarmale')).toBe('Sarmale');
+	test('Test for capitulized input ', () => {
+		expect(capitulize('Sarmale')).toBe('Sarmale');
+	});
 });
